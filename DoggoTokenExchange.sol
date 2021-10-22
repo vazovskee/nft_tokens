abc88f935046ae460bcf63d3155b5d250589773d
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-22 15:17:10 +0300

    function for putting a token for sale was added

--- DoggoTokenExchange.sol
+++ DoggoTokenExchange.sol
@@ -13,13 +13,22 @@ contract DoggoTokenExchange {
 
     mapping (string => DoggoToken) allTokens;  // сопоставление имени токена с самим токеном
     mapping (string => uint) tokenToOwner;     // сопоставление имени токена с его текущим владельцем
+    mapping (string => uint128) tokensForSale; // сопоставление имени выставленного на продажу токена с его ценой
 
-    function createToken(string name, string breed, string color, uint8 weight) public {
-        require(!name.empty(), 201, "Token's name cannot be empty");
-        require(!allTokens.exists(name), 202, "Token with this name already exists");
+    function createToken(string tokenName, string breed, string color, uint8 weight) public {
+        require(!tokenName.empty(), 201, "Token's name cannot be empty");
+        require(!allTokens.exists(tokenName), 202, "Token with this name already exists");
         tvm.accept();
 
-        allTokens[name] = DoggoToken(name, breed, color, weight); // создание нового токена
-        tokenToOwner[name] = msg.pubkey();                        // присваиваем новый токен владельцу
+        allTokens[tokenName] = DoggoToken(tokenName, breed, color, weight); // создание нового токена
+        tokenToOwner[tokenName] = msg.pubkey(); // присваиваем новый токен владельцу
+    }
+
+    function putTokenForSale(string tokenName, uint128 price) public {
+        require(allTokens.exists(tokenName), 203, "Token with this name doesn't exist");
+        require(msg.pubkey() == tokenToOwner[tokenName], 204, "Only owner of the token can put it for sale");
+        tvm.accept();
+        
+        tokensForSale[tokenName] = price; // выставляем цену токена, выставленного на продажу
     }
 }
