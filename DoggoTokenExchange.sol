701ab4862fa90bbfec75a245025b056c4aaacb0e
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-22 14:52:03 +0300

    function for creating a new token was added

--- DoggoTokenExchange.sol
+++ DoggoTokenExchange.sol
@@ -11,6 +11,15 @@ contract DoggoTokenExchange {
         uint8 weight;
     }
 
-    mapping (string => DoggoToken) tokens;  // имя токена выступает в качестве ключа
+    mapping (string => DoggoToken) allTokens;  // сопоставление имени токена с самим токеном
+    mapping (string => uint) tokenToOwner;     // сопоставление имени токена с его текущим владельцем
 
+    function createToken(string name, string breed, string color, uint8 weight) public {
+        require(!name.empty(), 201, "Token's name cannot be empty");
+        require(!allTokens.exists(name), 202, "Token with this name already exists");
+        tvm.accept();
+
+        allTokens[name] = DoggoToken(name, breed, color, weight); // создание нового токена
+        tokenToOwner[name] = msg.pubkey();                        // присваиваем новый токен владельцу
+    }
 }
