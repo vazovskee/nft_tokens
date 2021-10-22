a8ae0dce326c36ceb576a701b8ea2cd244059727
Ayvaz Miftakhov <ayvaz.miftakhov@gmail.com>
2021-10-22 14:32:37 +0300

    token structure created

--- DoggoTokenExchange.sol
+++ DoggoTokenExchange.sol
@@ -3,5 +3,14 @@ pragma ton-solidity >= 0.35.0;
 pragma AbiHeader expire;
 
 contract DoggoTokenExchange {
+    
+    struct DoggoToken {
+        string name;
+        string breed;
+        string color;
+        uint8 weight;
+    }
+
+    mapping (string => DoggoToken) tokens;  // имя токена выступает в качестве ключа
 
 }
